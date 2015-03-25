d1a60f7368afbd08314992a0b8bcc508dbbaa3e4
stanky <cristian.stancalau@garessio.ro>
2015-03-26 00:00:38 +0200

    Fixed access level bug on Metrics

--- src/main/java/ro/stancalau/pong/model/Metrics.java
+++ src/main/java/ro/stancalau/pong/model/Metrics.java
@@ -2,32 +2,32 @@ package ro.stancalau.pong.model;
 
 import java.util.Observable;
 
-public class Metrics extends Observable{
+public class Metrics extends Observable {
 
-	private double fps=0;
-	private double minFps = Double.MAX_VALUE;
-	private double maxFps = Double.MIN_VALUE;
+    private double fps = 0;
+    private double minFps = Double.MAX_VALUE;
+    private double maxFps = Double.MIN_VALUE;
 
-	public double getFps() {
-		return fps;
-	}
+    public double getFps() {
+        return fps;
+    }
 
-	protected void setFps(double fps) {
-		if (this.fps==fps || fps<0 || fps==Double.POSITIVE_INFINITY) return;
-		this.fps = fps;
+    public void setFps(double fps) {
+        if (this.fps == fps || fps < 0 || fps == Double.POSITIVE_INFINITY) return;
+        this.fps = fps;
 
-		if (fps<minFps) minFps = fps;
-		if (fps>maxFps) maxFps = fps;
+        if (fps < minFps) minFps = fps;
+        if (fps > maxFps) maxFps = fps;
 
-		setChanged();
-		notifyObservers();
-	}
+        setChanged();
+        notifyObservers();
+    }
 
-	public double getMinFps() {
-		return minFps;
-	}
+    public double getMinFps() {
+        return minFps;
+    }
 
-	public double getMaxFps() {
-		return maxFps;
-	}
+    public double getMaxFps() {
+        return maxFps;
+    }
 }
